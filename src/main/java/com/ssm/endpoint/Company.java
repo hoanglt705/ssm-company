61a187aa5042f8c0dfc47d5c45c7e0a34d9737c4
hoanglt705 <hoanglt705@gmail.com>
2017-12-03 23:55:55 +0900

    Change

---
+++ src/main/java/com/ssm/endpoint/Company.java
@@ -0,0 +1,28 @@
+package com.ssm.endpoint;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("company")
+public class Company {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getIt() {
+      CompanyResponse response = new CompanyResponse();
+      response.setName("Hoang Le");
+        return Response.ok().entity("Hello").build();
+    }
+}
---
+++ src/main/java/com/ssm/endpoint/CompanyResponse.java
@@ -0,0 +1,14 @@
+package com.ssm.endpoint;
+
+public class CompanyResponse {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
